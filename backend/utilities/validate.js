405de602b2d62fc488bc6028746731a1e5ca597c
RomDmitriy <aisek72@gmail.com>
2023-01-06 19:44:50 +0300

    Добавлена документация к валидатору

--- backend/utilities/validate.js
+++ backend/utilities/validate.js
@@ -1,5 +1,13 @@
 import { validationResult } from "express-validator";
+import pkg from 'express';
+const { Request, Response, NextFunction } = pkg;
 
+/**
+ * Проверка валидаторов, написанных в прошлом middleware
+ * @param { Request } req 
+ * @param { Response } res 
+ * @param { NextFunction } next 
+ */
 export default function validate(req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
