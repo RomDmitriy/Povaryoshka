b97d4028d7de8c0c5bfd65b98b2a5c5097f7585a
RomDmitriy <aisek72@gmail.com>
2023-01-06 13:08:37 +0300

    Добавлена валидация логина. Валидации пропускаются

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller.js';
-import { registerValidation } from '../validations/auth.validator.js';
+import { loginValidation, registerValidation } from '../validations/auth.validator.js';
 
 const authRouter = Router();
 const authController = new AuthController();
 
+//TODO: валидация почему-то пропускается
 authRouter.post('/register', registerValidation, authController.registerUser);
 
-authRouter.post('/login', authController.loginUser);
+authRouter.post('/login', loginValidation, authController.loginUser);
 
 export default authRouter;
--- backend/validations/auth.validator.js
+++ backend/validations/auth.validator.js
@@ -3,6 +3,11 @@ import { body } from 'express-validator';
 export const registerValidation = [
     body('email').isEmail(),
     body('password').isLength({min: 6, max: 32}),
-    body('fullName').split(' ').length === 3,
-    body('avatarUrl').optional().isURL()
+    body('fullName').isLength({min: 3, max: 64}),
+    body('avatarUrl').optional().isURL(),
+];
+
+export const loginValidation = [
+    body('email').isEmail(),
+    body('password').isLength({min: 6, max: 32}),
 ];
