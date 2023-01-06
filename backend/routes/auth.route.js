787b526c6a50a37a6c2d6b66602f62facc3f7632
RomDmitriy <aisek72@gmail.com>
2023-01-06 12:26:01 +0300

    Добавлена валидация при регистрации

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller.js';
+import { registerValidation } from '../validations/auth.validator.js';
 
 const authRouter = Router();
 const authController = new AuthController();
 
-authRouter.post('/register', authController.registerUser);
+authRouter.post('/register', registerValidation, authController.registerUser);
 
 authRouter.post('/login', authController.loginUser);
 
