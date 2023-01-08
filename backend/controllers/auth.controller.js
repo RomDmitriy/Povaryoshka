56080ff4616e48851d00ecfc530663936a4dee1e
RomDmitriy <aisek72@gmail.com>
2023-01-08 22:11:21 +0300

    Добавлена обработка если почта уже занята

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,21 +5,26 @@ import * as bcrypt from 'bcrypt';
 
 export class AuthController {
     async registerUser(req, res) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const passwordHash = await bcrypt.hash(req.body.password, salt);
 
-        const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(req.body.password, salt);
+            const doc = new UserModel({
+                email: req.body.email,
+                password: passwordHash,
+                fullName: req.body.fullName,
+                avatarUrl: req.body.avatarUrl
+            });
 
-        const doc = new UserModel({
-            email: req.body.email,
-            password: passwordHash,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl
-        });
+            const user = await doc.save();
 
-        const user = await doc.save();
-
-        const tokens = generateTokens({id: doc._id});
-        res.json(tokens);
+            const tokens = generateTokens({id: user._id});
+            res.json(tokens);
+        } catch (err) {
+            if (err.code === 11000) {
+                res.status(409).end();
+            }
+        }
     }
 
     async loginUser(req, res) {
